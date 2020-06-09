67a39613302df9054889a510766cd38c6eeff3a9
kaushik Seetharam <kaushikseetharam989@gmail.com>
2020-06-09 11:47:40 +0530

    commit

--- Programming/encryption_decryption/Website_unciphered.py
+++ Programming/encryption_decryption/Website_unciphered.py
@@ -1,9 +1,13 @@
 from cryptography.fernet import Fernet
 import os
 import sys
+cmd = 'git clone git@github.com:skaushikpowershell/Key.git'
+os.system(cmd)
+encryppasswd = b'gAAAAABe3kUIbTvxaNqhI9lQGtwrrzJsHiBHUb-crCqn2lTxMxSs37zM8CyLJG1qLI7Iy1dRMpP9VabSJSlzPPzs-nkldIcIsS7gYBk-5pEuWfs2XtFmFV0='
+print (encryppasswd)
 key = b'pRmgMa8T0INjEAfksaq2aafzoZXEuwKI7wDe4c1F8AY='
 cipher_suite = Fernet(key)
-EncryptedPasswd = b'gAAAAABe3kUIbTvxaNqhI9lQGtwrrzJsHiBHUb-crCqn2lTxMxSs37zM8CyLJG1qLI7Iy1dRMpP9VabSJSlzPPzs-nkldIcIsS7gYBk-5pEuWfs2XtFmFV0='
+EncryptedPasswd = encryppasswd
 unciphered_text = (cipher_suite.decrypt(EncryptedPasswd))
 string_unciphered_text = str(unciphered_text)
 string_unciphered_text = string_unciphered_text.replace("'", "")
@@ -13,9 +17,4 @@ if filtered_string_unciphered_text == "SuperSecretPassword":
 else:
     print ('failure')
 
-try:  
-   filtered_string_unciphered_text = os.environ["Passwd"] 
-   print (filtered_string_unciphered_text)
-except KeyError: 
-   print ("Please set the environment variable NEWVAR")
-   sys.exit(1)
+
